1bb3130efc7c0e22a8c1632793dcba6802315b7d
Alex Jones <atjalex@gmail.com>
2024-09-21 15:00:42 -0700

    some WIP files for remote agents and DAMS notes

---
+++ src/agent_remote.rs
@@ -0,0 +1,13 @@
+/* 
+    A remote attachment point from agent-to-agent.
+
+    A set of agents with the same messaging profile can share a single attachment point.
+
+    Question: can we mix remote and local attachments between agents?
+    
+    We could recreate attachment points.
+
+    
+ */
+
+
---
+++ src/dams.rs
@@ -0,0 +1,16 @@
+/*
+    Goal for this module.
+
+    1. Develop an agent program built using agent factories.
+    2. Make agents a composition of 4 systems: DAMS
+        Data, Messaging, Scheduling, Actions
+    3. Ensure data flow goes from M->D, D<->S, S->A
+        Messaging system transforms incoming messages and delivers them to the Data system
+        Data system and Scheduling system 
+    4. Connect agents D,A,M,S; D',A',M',S' via a connection between A -> M'
+ */
+
+ /*
+    What code do we have already?
+        I've made several partial versions of similar systems. In this repo are a set of modules used to develop
+  */
